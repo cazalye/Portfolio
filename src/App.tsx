ac3384a73f95fa31c1f8acf83285adcf459e30c5
Emma <e.cazaly7@gmail.com>
2020-05-06 13:30:13 +1000

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+// import './App.css';
+import Navbar from './components/navbar/navbar';
+import View from './components/view';
+import Footer from './components/footer/footer';
+import { withRouter } from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any,any> {
+
+  render() {  
+    return (
+      <div className="App">
+          <Navbar/>
+          <View/>
+          <Footer/>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withRouter(App);
