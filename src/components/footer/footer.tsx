800efa411aa9d159ca92a7f36a6fbad7e297d888
Emma <e.cazaly7@gmail.com>
2020-05-06 14:17:54 +1000

    Update footer links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,17 +5,26 @@ class Footer extends Component{
     render(){
         return(
             <div className="footer-links">
-                <a href="https://www.instagram.com/cazalye" rel="noopener noreferrer" target="_blank">
+                {/* <a href="https://www.instagram.com/cazalye" rel="noopener noreferrer" target="_blank">
                     <i className="fab fa-instagram" aria-hidden="true" />
                 </a>
                 <a href="https://www.pinterest.com.au/cazalye/" rel="noopener noreferrer" target="_blank">
                     <i className="fab fa-pinterest-p" aria-hidden="true" />
-                </a>
+                </a> */}
                 <a href="https://github.com/cazalye" rel="noopener noreferrer" target="_blank">
                     <i className="fab fa-github" aria-hidden="true" />
                 </a>
                 <a href="mailto:e.cazaly7@gmail.com" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-envelope" aria-hidden="true" />
+                </a>
+                {/* <a href="tel:+358417077974" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-phone" aria-hidden="true" />
+                </a> */}
+                <a href="tel:+61421698344" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-phone" aria-hidden="true" />
+                </a>
+                <a href="https:cazalye.com" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-globe" aria-hidden="true" />
                 </a>
             </div>
         );
