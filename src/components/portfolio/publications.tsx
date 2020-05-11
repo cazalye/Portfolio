ab838793e7a0b54539daec608a556863f859d47d
Emma <e.cazaly7@gmail.com>
2020-05-11 16:27:26 +1000

    Update publications

--- src/components/portfolio/publications.tsx
+++ src/components/portfolio/publications.tsx
@@ -1,19 +1,26 @@
 import React, { Component} from 'react';
 import {Grid, Cell} from 'react-mdl';
 
-// class Publications extends Component {
-//     render() {
-//         return(
-//             <Grid className='publication-year'>
-//                 <Cell col={4}>
-//                     <p>{this.props.publicationYear}</p>
-//                 </Cell>
-//                 <Cell col={8}>
-//                     <a href={this.props.publicationLink} target="_blank" rel="noopener noreferrer">{this.props.publicationTitle}</a>
-//                 </Cell>
-//             </Grid>
-//         )
-//     }
-// }
+interface PublicationProps{
+    publicationYear: number;
+    publicationLink?: string;
+    publicationTitle: string;
+    publicationJournal: string;
+}
+class Publications extends Component<PublicationProps, PublicationProps> {
+    render() {
+        return(
+            <Grid className='publication-year'>
+                <Cell col={4}>
+                    <p>{this.props.publicationYear}</p>
+                </Cell>
+                <Cell col={8}>
+                    <p>{this.props.publicationJournal}</p>
+                    <a href={this.props.publicationLink} target="_blank" rel="noopener noreferrer">{this.props.publicationTitle}</a>
+                </Cell>
+            </Grid>
+        )
+    }
+}
 
-// export default Publications;
+export default Publications;
