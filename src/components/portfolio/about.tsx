3f28e77e59b3e0565620a734bdf8e1ade8742a70
Emma <e.cazaly7@gmail.com>
2020-05-12 23:36:17 +1000

    Update about content, image and style

--- src/components/portfolio/about.tsx
+++ src/components/portfolio/about.tsx
@@ -6,19 +6,13 @@ class About extends Component {
     render() {
         return(
             <div className="aboutme-background">
+                <div className="aboutme-image"> 
+                    <img className="me-portrait" src="/img/me_portrait2.jpg" height="600px" width="auto" alt="portrait of me"/>
+                </div>
                 <div className="aboutme-text">
                     <h2>I'm a designer and front-end developer with a passion for photography and a background in data science. </h2>
                     <p>I create evocative travel websites, catchy copy and enticing e-commerce sites. I have a solid knowledge of the entire web development process and leverage my analytical skills and strong attention to detail to deliver highly effective, pixel perfect web solutions. I build new websites from start to finish and specialise in: </p>
-                    <ul>    
-                        <li> Discussing vision, scope, style and ideas with clients</li>
-                        <li> Web Design including wireframing and prototyping</li>
-                        <li> Front End Development including JavaScript/TypeScript, HTML, CSS (SASS)</li>
-                        <li> Deployment including hosting & domain name, site security, payment gateways</li>
-                        <li> User Experience including Usability testing</li>
-                        <li> SEO optimisation</li>
-                    </ul>
-                    {/* <p> I hold both Australian and Italian citzenship.</p> */}
-                    {/* <p>Visit me at <a href="https://www.cazalye.com">cazalye.com</a></p> */}
+                        
                 </div>
                 <Footer/>
             </div>
