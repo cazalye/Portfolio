25d6231e47cbf37aaf6beb46cf7f93a52e35df0e
Emma <e.cazaly7@gmail.com>
2020-05-13 14:55:30 +1000

    Update blog page

--- src/components/portfolio/travelBlog.tsx
+++ src/components/portfolio/travelBlog.tsx
@@ -5,8 +5,31 @@ class TravelBlog extends Component {
     render() {
         return (
             <div id="project-details">
+                <div className="feature-image">
+                    <img src="/img/Rafting.jpg" height="auto" width="100%" alt="cazalye travel blog"/>
+                </div>
+                {/* <Breadcrumbs maxItems={4} aria-label="breadcrumb" className="breadcrumbs">
+                        <Link to="/" color="inherit">
+                            Home
+                        </Link>
+                        <Link to="/photoDiaries" color="inherit">
+                            Photo Diaries
+                        </Link>
+                    </Breadcrumbs> */}
+                <div className="project-summary"> 
+                    <h2>Travel Blog: cazalye</h2>
+                    {/* <h2 className="sub-header">Brands on Parade Online Bottle Shop</h2> */}
+                    <h3>Client</h3>
+                    <p>My own passion project</p>
+                    <h3>Date</h3>
+                    <p>Nov 2019 - Feb 2020</p>
+                    <h3>My Role</h3>
+                    <p>Design, Development, Photography, Content Creation</p>
+                    <h3>Check it out</h3>
+                    <a href="http://cazalye.com"> Launch Site</a>
+                </div>
                 <div className="project-content">
-                    <h2>Travel Blog: cazalye.com</h2>
+                    {/* <h2>Travel Blog: cazalye.com</h2> */}
                     <h3>The Brief</h3>
                     <p>To create a visually rich, exciting and inspiring travel blog with two main elements: a unique way to display "photo diaries" and a clean modern way to display blog posts. Both aspects were to be asthetically pleasing in a minimalist style. It was also key to have an engaging About Page and a Newsletter Sign Up Form</p>
                     <h3>The Approach</h3>
@@ -14,7 +37,8 @@ class TravelBlog extends Component {
                     <h3>My Role</h3>
                     <p>For this passion project I collaborated with a full stack developer who had 4 years experience leading large projects, particularly e-commerce sites. The first month of the projet was face-to-face, after which I moved back to Australia and predominantly worked on the site myself. We had regular remote coding sessions for bug fixes and adding new elements- for this we used Visual Studio Code's Live Share feature, including sharing the development server. Throughout the project git/GitHub was used for version control and to share code.</p>
                     <h3>The Results</h3>
-                    <p>What was delivered, testimonial, increased sales, link website</p>
+                    <p>An exciting, visually rich travel blog that reflects my personal style and provides me with a unique platform to share my travel photography and writing</p>
+                    <a href="http://cazalye.com"> Visit Site</a>
                 </div>
             </div>
         );
