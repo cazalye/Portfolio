885b78032e8b233da10c6fbbd6a7bb762fe51d8f
Emma <e.cazaly7@gmail.com>
2020-05-17 09:22:01 +1000

    Add Quentin font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-// import './App.css';
+import './styles/App.scss';
 import Navbar from './components/navbar/navbar';
 import View from './components/view';
 import Footer from './components/footer/footer';
