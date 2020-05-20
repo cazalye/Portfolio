3799e16c5d69ae6ceb5003877f6fca3b0dae3c41
Emma <e.cazaly7@gmail.com>
2020-05-21 08:05:36 +1000

    Simplify project cards

--- src/components/portfolio/projectCards.tsx
+++ src/components/portfolio/projectCards.tsx
@@ -19,15 +19,21 @@ class ProjectCards extends Component<ProjectCardProps,ProjectCardProps> {
         return (
             <div className="project-cards">
                 <div className="content">
+                   
+                    <Link className="project-link" to={this.props.more}>
                     <h2>{this.props.title}</h2>
                     <p>{this.props.summary}</p>
-                    <Link className="project-link" to={this.props.more}>Project Details</Link>
+                    {/* <p>Details</p> */}
+                    </Link>
+
+
                     <a href={this.props.gitLink} rel="noopener noreferrer" target="_blank">
                         <i className={this.props.gitIcon} aria-hidden="true"/>
                     </a>
                     <a href={this.props.siteLink} rel="noopener noreferrer" target="_blank">
                         <i className={this.props.siteIcon} aria-hidden="true"/>
                     </a>
+
                 </div>
             </div>
         );
