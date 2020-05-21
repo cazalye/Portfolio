e65f40044c5fd9a5cccfcbecbe7c48893b89eeb7
Emma <e.cazaly7@gmail.com>
2020-05-21 22:16:53 +1000

    Edit about bullet points

--- src/components/portfolio/about.tsx
+++ src/components/portfolio/about.tsx
@@ -12,14 +12,15 @@ class About extends Component {
                 <div className="aboutme-text">
                     <h2 className="title-long">I'm a front-end React developer with a passion for photography and design.</h2>
                     <h2 className="title-short">A <br/>designer <br/>who <br/> codes</h2>
-                    <p>I combine my analytical skills and strong attention to detail, honed from my career in data science, with modern development tools and an understanding of UX Design, to build effective, pixel perfect web solutions from start to finish. </p>
+                    <p>I combine my analytical skills and strong attention to detail, honed from my career in data science, with modern development tools and an understanding of UX Design, to build effective, pixel perfect web solutions from start to finish. My experience includes:</p>
                         <ul>
                             <li> Discussing vision, scope and style with clients</li>
                             <li> Web Design including wireframing and prototyping</li>
-                            <li> Front-End Development including JavaScript/TypeScript, HTML, CSS (SASS)</li>
-                            <li> Deployment including hosting & domain names, site security, payment gateways</li>
+                            <li> Front-End Development: React, JavaScript/TypeScript, HTML, CSS (SASS)</li>
+                            <li> WordPress and  WooCommerce</li>
+                            <li> Deployment, including hosting & domain names, site security, payment gateways</li>
                             <li> User Experience including Usability testing</li>
-                            <li> SEO optimisation</li>
+                            <li> SEO, Google Indexing & Google Analytics</li>
                         </ul>
 
 
