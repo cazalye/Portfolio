378837306dd95c524e515db8bae156e8c5db23d8
Emma Cazaly <cazalye@Emma-iMac.home>
2020-06-12 11:17:31 +1000

    Icon for Brands

--- src/components/portfolio/experience.tsx
+++ src/components/portfolio/experience.tsx
@@ -10,6 +10,7 @@ interface ExperienceProps {
     jobDescription: string;
     url?: string;
     site?: string;
+    icon?: string;
 }
 
 
@@ -24,7 +25,10 @@ class Experience extends Component<ExperienceProps, ExperienceProps>{
                     <h4>{this.props.jobName}</h4>
                     <p>{this.props.project}</p>
                     <p>{this.props.jobDescription}</p>
-                    <a href={this.props.url} target="_blank" rel="noopener noreferrer">{this.props.site}</a>
+                    <a href={this.props.url} target="_blank" rel="noopener noreferrer">
+                        {this.props.site}
+                        <i className={this.props.icon}/>
+                    </a>
                 </Cell>
             </Grid>
         )
