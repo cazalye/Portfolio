90897f266fb9d21e01460692b12076bbef0ceb25
Emma <e.cazaly7@gmail.com>
2020-06-13 14:43:18 +1000

    Switch telephone for LinkedIn in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -14,15 +14,18 @@ class Footer extends Component{
                 <a href="https://github.com/cazalye" rel="noopener noreferrer" target="_blank">
                     <i className="fab fa-github" aria-hidden="true" />
                 </a>
+                <a href="https://www.linkedin.com/in/emmacazaly/" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin" aria-hidden="true" />
+                </a>
                 <a href="mailto:e.cazaly7@gmail.com" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-envelope" aria-hidden="true" />
                 </a>
                 {/* <a href="tel:+358417077974" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-phone" aria-hidden="true" />
                 </a> */}
-                <a href="tel:+61421698344" rel="noopener noreferrer" target="_blank">
+                {/* <a href="tel:+61421698344" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-phone" aria-hidden="true" />
-                </a>
+                </a> */}
                 {/* <a href="http://cazalye.com" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-globe" aria-hidden="true" />
                 </a> */}
